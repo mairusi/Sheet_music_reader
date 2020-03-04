840d22de63c9abfa0e764458417f9a75523f0252
Suthika Wongsiridech <61772819+mairusi@users.noreply.github.com>
2020-03-04 13:46:17 +0700

    add drive api

---
+++ Mask_RCNN/Drive_API/drive_api_write_file.py
@@ -0,0 +1,63 @@
+from __future__ import print_function
+import pickle
+import os.path
+import io
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.http import MediaIoBaseDownload
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+def downloadFile( service, file_id, filepath ):
+    request = service.files().get_media(fileId=file_id)
+    fh = io.BytesIO()
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        print("Download %d%%." % int(status.progress() * 100))
+    with io.open(filepath,'wb') as f:
+        fh.seek(0)
+        f.write(fh.read())
+
+def searchFile( service, size, query ):
+    results = service.files().list(
+    pageSize=size,fields="nextPageToken, files(id, name, kind, mimeType)",q = "name contains '{}'". format(query)).execute()
+    items = results.get('files', [])
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print(item)
+            print('{0} ({1})'.format(item['name'], item['id']))
+            return item['id']
+
+############################ MAIN #############################
+
+creds = None
+# The file token.pickle stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+if os.path.exists('token.pickle'):
+    with open('token.pickle', 'rb') as token:
+        creds = pickle.load(token)
+# If there are no (valid) credentials available, let the user log in.
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open('token.pickle', 'wb') as token:
+        pickle.dump(creds, token)
+
+service = build('drive', 'v3', credentials=creds)
+
+file_id = searchFile(service, 1, 'music_notation_location')
+
+downloadFile( service, file_id, 'music_notation_location.txt')
